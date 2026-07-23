af371afd82b7e152adb486780b2f2e8b301f5ce7
Dhananjay <itsmrnatural@duck.com>
2026-07-23 16:20:26 +0530

    chore: upgrade deps to latest major versions
    - Next.js 16.2.11, React 19.2.8, framer-motion 12, SWR 2
    - Tailwind CSS 4.3.3, ESLint 10.7.0, Prettier 3.9.6
    - Remove unused deps (html2pdf, jspdf, webpack plugins, @next/mdx, etc.)
    - Add pnpm override eslint-plugin-import -> eslint-plugin-import-x

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,6 @@
-import mdx from "@next/mdx";
-import remarkGfm from "remark-gfm";
-import remarkFootnotes from "remark-footnotes";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeHighlight from "rehype-highlight";
-import TerserPlugin from "terser-webpack-plugin";
-
-const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm, remarkFootnotes],
-    rehypePlugins: [
-      rehypeSlug,
-      rehypeAutolinkHeadings,
-      rehypeHighlight,
-    ],
-  },
-});
-
-export default withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
@@ -36,32 +17,16 @@ export default withMDX({
       },
       {
         protocol: "https",
+        hostname: "media.discordapp.net",
+      },
+      {
+        protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
     ],
   },
-  compress: true,
-  productionBrowserSourceMaps: false,
-  webpack: (config, { isServer }) => {
-    config.mode = "production";
-
-    if (!isServer) {
-      config.optimization.minimizer.push(
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
-            output: {
-              comments: false,
-            },
-          },
-        })
-      );
-    }
-
-    return config;
-  },
   reactStrictMode: true,
   poweredByHeader: false,
-});
+};
+
+export default nextConfig;
